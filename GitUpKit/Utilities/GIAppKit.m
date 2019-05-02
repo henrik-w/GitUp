0cb3df3ad47712a6f17a90d330d16813355de6b1
hackmac <henrik.w@gmx.de>
2019-05-02 21:35:41 +0200

    GitUpKit: Fixed deprecation warnings for builds against macOS later than 10.8.

--- GitUpKit/Utilities/GIAppKit.m
+++ GitUpKit/Utilities/GIAppKit.m
@@ -53,6 +53,14 @@ static NSColor* _separatorColor = nil;
 
 @implementation NSAlert (GIAppKit)
 
+#if MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_9
+
+- (void)beginSheetModalForWindow:(NSWindow*)window withCompletionHandler:(void (^)(NSModalResponse returnCode))handler {
+  [self beginSheetModalForWindow:window completionHandler:handler];
+}
+
+#else
+
 + (void)_alertDidEnd:(NSAlert*)alert returnCode:(NSInteger)returnCode contextInfo:(void*)contextInfo {
   void (^handler)(NSInteger) = contextInfo ? CFBridgingRelease(contextInfo) : NULL;
   [alert.window orderOut:nil];  // Dismiss the alert window before the handler might chain another one
@@ -64,6 +72,8 @@ static NSColor* _separatorColor = nil;
 - (void)beginSheetModalForWindow:(NSWindow*)window withCompletionHandler:(void (^)(NSInteger returnCode))handler {
   [self beginSheetModalForWindow:window modalDelegate:[NSAlert class] didEndSelector:@selector(_alertDidEnd:returnCode:contextInfo:) contextInfo:(handler ? (void*)CFBridgingRetain(handler) : NULL)];
 }
+
+#endif
 
 - (void)setType:(GIAlertType)type {
   switch (type) {
